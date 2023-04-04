974af815e2e5badf019b807dccaa8b327a093464
mubarak <mubarakcali24@gmail.com>
2023-04-03 19:56:53 -0700

    cleaned up

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,6 @@
 var giphykey = 'XdKAq0Iih87Sx83LInGdrQgoMit3ALUH'
 const searchBtn = document.getElementById("generateGif");
-const Character = document.getElementById();
-
-//const Character = document.getElementById();
+const Character = document.getElementById("monday");
 const tyrion = 'https://api.gameofthronesquotes.xyz/v1/author/tyrion/2'
 const Tywin = 'https://api.gameofthronesquotes.xyz/v1/author/tywin/2'
 const daenerys = 'https://api.gameofthronesquotes.xyz/v1/author/daenerys/2'
@@ -10,9 +8,8 @@ const jon = 'https://api.gameofthronesquotes.xyz/v1/author/jon/2'
 const Arya = 'https://api.gameofthronesquotes.xyz/v1/author/arya/2'
 const jaime = 'https://api.gameofthronesquotes.xyz/v1/author/jaime/2'
 const Cersei = 'https://api.gameofthronesquotes.xyz/v1/author/cersei/2'
-//FLAGS
 
-//Event listerners
+//Event Listener
 searchBtn.addEventListener("click", function () {
 
     fetch('https://api.gameofthronesquotes.xyz/v1/author/tyrion/2')
