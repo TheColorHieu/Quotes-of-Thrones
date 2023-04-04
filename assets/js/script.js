09947eba8ac03d30f0a14fb37ea72079f55c9fb2
adamjfox <adamjeffersonfox@gmail.com>
2023-04-03 19:32:19 -0700

    clean up 1

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,22 @@
 // API
 //quotes url = https://api.gameofthronesquotes.xyz/v1/author/tyrion/2
-//its by character?
 //giphy key = XdKAq0Iih87Sx83LInGdrQgoMit3ALUH
-//quote key =
-//Consts
-//const searchBtn = document.getElementById();
-//const searchBtn = document.getElementById();
-//const searchBtn = document.getElementById();
+
 //const Character = document.getElementById();
-//const tyrion = https://api.gameofthronesquotes.xyz/v1/author/tyrion/2
-//const Tywin = https://api.gameofthronesquotes.xyz/v1/author/
-//const Daenerys = https://api.gameofthronesquotes.xyz/v1/author/
-//const The Hound = https://api.gameofthronesquotes.xyz/v1/author/
-//Arya = https://api.gameofthronesquotes.xyz/v1/author/
-//Peter Balish = https://api.gameofthronesquotes.xyz/v1/author/
+const tyrion = 'https://api.gameofthronesquotes.xyz/v1/author/tyrion/2'
+const Tywin = 'https://api.gameofthronesquotes.xyz/v1/author/tywin/2'
+const daenerys = 'https://api.gameofthronesquotes.xyz/v1/author/daenerys/2'
+const jon = 'https://api.gameofthronesquotes.xyz/v1/author/jon/2'
+const Arya = 'https://api.gameofthronesquotes.xyz/v1/author/arya/2'
+const jaime = 'https://api.gameofthronesquotes.xyz/v1/author/jaime/2'
+const Cersei = 'https://api.gameofthronesquotes.xyz/v1/author/cersei/2'
 //FLAGS
 
 //Event listerners
 searchBtn.addEventListener('click')
 
-//function getcharacter(){
-
-//fetch()
 
 
-//}
 
 
 
