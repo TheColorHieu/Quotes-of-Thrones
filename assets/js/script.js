76f077deac3a51442a3611cba15a885ecd1f5ef2
Hieu Tran <hieuthanhtran98@yahoo.com>
2023-04-03 19:56:44 -0700

    added the giphy function

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,7 +17,9 @@
 //FLAGS
 
 //Event listerners
-searchBtn.addEventListener('click')
+$("#b1,#b2,#b3,#b4,#b5,#b6,#b7").bind("click",function(){
+    console.log("clicky clacky");
+})
 
 //function getcharacter(){
 
@@ -29,3 +31,27 @@ searchBtn.addEventListener('click')
 
 
 //Console.log
+let APIKEY = XdKAq0Iih87Sx83LInGdrQgoMit3ALUH;
+document.addEventListener("DOMContentLoaded", init);
+function init (){
+    document.getElementById("btnSearch").addEventListener("click", ev => {
+        //preventing the website to refresh 
+        ev.preventDefault();
+        let url = 'https://api.giphy.com/v1/gifs/search?api_key=${APIKEY}&limit=1&q=';
+        let str = document.getElementById("search").ariaValueMax.trim();
+        url = url.concat(str);
+        coknsole.log(url);
+        fetch(url)
+        .then(response => response.json() )
+        .then(content => {
+            //data
+            console.log(content.data);
+            cosole.log('META', content.meta);
+           
+        })
+        .catch(err=>{
+            console.error(err);
+
+        })
+    });
+}
