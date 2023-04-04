e74931201d300f13a19e33bedf9c0f0374255d09
adamjfox <adamjeffersonfox@gmail.com>
2023-04-03 17:39:18 -0700

    progress 1

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,19 @@
 // API
+//quotes url = https://api.gameofthronesquotes.xyz/v1/author/tyrion/2
+//its by character?
 //giphy key = XdKAq0Iih87Sx83LInGdrQgoMit3ALUH
 //quote key =
-//SELECTORS
-
-
+//Consts
+//const searchBtn = document.getElementById();
+//const searchBtn = document.getElementById();
+//const searchBtn = document.getElementById();
+//const Character = document.getElementById();
 //FLAGS
 
+//Event listerners
+searchBtn.addEventListener('click')
+
+//function getcharacter(){
+
+
+}
