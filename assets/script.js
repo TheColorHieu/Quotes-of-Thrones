1b079d00dd0419ed2e5d11c4ab4fa7840f9c9c07
adamjfox <adamjeffersonfox@gmail.com>
2023-04-03 18:03:18 -0700

    progress 3

--- assets/script.js
+++ assets/script.js
@@ -9,11 +9,11 @@
 //const searchBtn = document.getElementById();
 //const Character = document.getElementById();
 //const tyrion = https://api.gameofthronesquotes.xyz/v1/author/tyrion/2
-//const Tywin
-//const Daenerys
-//const The Hound
-//Arya
-//Peter Balish
+//const Tywin = https://api.gameofthronesquotes.xyz/v1/author/
+//const Daenerys = https://api.gameofthronesquotes.xyz/v1/author/
+//const The Hound = https://api.gameofthronesquotes.xyz/v1/author/
+//Arya = https://api.gameofthronesquotes.xyz/v1/author/
+//Peter Balish = https://api.gameofthronesquotes.xyz/v1/author/
 //FLAGS
 
 //Event listerners
